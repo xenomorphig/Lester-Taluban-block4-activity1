94e9a95b30697bb7a9ae54453ed3e20dc1ba482a
xenomorphig <152960996+xenomorphig@users.noreply.github.com>
2023-12-07 12:46:32 +0800

    UP-FC1-BSIT2-04
    Hi

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "LesterTaluban-Block4-Quiz"
+include(":app")
+ 
